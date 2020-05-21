f3b4b15751f15156e3f34dc8f7fa3182213bd605
Gugabit <52040719+Gugabit@users.noreply.github.com>
2020-05-21 20:09:22 -0300

    Update backupScript.sh

--- backupScript.sh
+++ backupScript.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 # Script Para Backup De banco De Dados SQL Server + SQLCMD Para Amazon AWS
 
-# Nomes Dos Bancos Que Serão Efetuados Os Backups Separados Por Espaço
-declare -a BANCO_NOMES=()
+# Exibe a data para fins de organizaç~ao no arquivo de log
+echo " "
+echo "-----------------------------------------------------------------------------"
+echo "------------------- $(date) -------------------"
 
-echo "$(date)"
 # Inicio Do Backup
-for Key in ${BANCO_NOMES[@]};
-	do echo "Backup $Key"; /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$DB_PASSWORD" -Q "BACKUP DATABASE $Key TO DISK = N'/var/opt/mssql/data/backups/$Key.bak' WITH NAME = '$Key'";
-done
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$DB_PASSWORD" -Q "\
+	DECLARE @name VARCHAR(50) \
+	DECLARE @path VARCHAR(256) \
+	DECLARE @fileName VARCHAR(256) \
+	DECLARE @fileDate VARCHAR(20) \
+	SET @path = '/var/opt/mssql/data/backups/' \
+	DECLARE db_cursor CURSOR FOR \
+	SELECT name \
+	FROM master.dbo.sysdatabases \
+	WHERE name NOT IN ('master','model','msdb','tempdb') \
+	OPEN db_cursor \
+	FETCH NEXT FROM db_cursor INTO @name \
+	WHILE @@FETCH_STATUS = 0 \
+	BEGIN \
+		SET @fileName = @path + @name + '.bak' \
+		BACKUP DATABASE @name TO DISK = @fileName \
+		FETCH NEXT FROM db_cursor INTO @name \
+	END \
+	CLOSE db_cursor \
+	DEALLOCATE db_cursor";
 
 # Compactação .ZIP
 sudo rm /home/ubuntu/backup/backup.zip
@@ -17,4 +35,5 @@ sudo zip -r /home/ubuntu/backup/backup.zip /var/opt/mssql/data/backups
 # Upload Para Bucket AWS S3
 sudo aws s3 cp /home/ubuntu/backup/backup.zip s3://$BACKUP_BUCKET/backup-$CUSTOMER-$(date +%Y%m%d-%H%M).zip
 
+# Reinicia o servidor ao fim do backup
 sudo reboot
