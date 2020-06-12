926faf46f01d4b749441bbb985b8ac9a8b72f343
Gugabit <52040719+Gugabit@users.noreply.github.com>
2020-06-12 15:16:50 -0300

    Update logStatus.sh

--- logStatus.sh
+++ logStatus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATA="$( df; echo "---break----------------------------------------------------"; ls -l /var/opt/mssql/data/; echo "---break----------------------------------------------------"; systemctl status mssql-server; | tr '\r\n' ' ')"
+DATA="$( (df; echo "---break----------------------------------------------------"; ls -l /var/opt/mssql/data/; echo "---break----------------------------------------------------"; systemctl status mssql-server;) | tr '\r\n' ' ')"
 
 curl --location --request POST "https://spitzer-monitor.herokuapp.com/status-set/$CUSTOMER" \
 --header 'Content-Type: application/json' \
