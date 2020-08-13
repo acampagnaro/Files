753a14923c42d78e0ada7dc0efcc9af0dfa512d2
Gustavo B <52040719+Gugabit@users.noreply.github.com>
2020-08-13 16:35:27 -0300

    Update getUpdates.sh

--- getUpdates.sh
+++ getUpdates.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # This scipt runs every 30 minutes to get and apply updates. Use with CAUTION!
 # Este script executa a cada 30 minutos buscando e aplicando atualizações. Use com CUIDADO!
-
-
-echo "0 0-23/12 * * * wget -qO- https://raw.githubusercontent.com/Gugabit/Files/master/backupScript.sh | sh >> /home/ubuntu/logs/backup.log" >> mycron
-echo "*/30 * * * * wget -qO- https://raw.githubusercontent.com/Gugabit/Files/master/getUpdates.sh | sh >> /home/ubuntu/logs/update.log" >> mycron
-echo "*/10 * * * * wget -qO- https://raw.githubusercontent.com/Gugabit/Files/master/logStatus.sh | sh" >> mycron
-echo "0 3 * * * sudo reboot" >> mycron
-crontab mycron
-rm mycron
-crontab -l
