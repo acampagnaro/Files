8499663ddfc78baf53a151713b9556a49007dfc6
Gustavo B <52040719+Gugabit@users.noreply.github.com>
2020-11-12 12:35:20 -0300

    Update getUpdates.sh

--- getUpdates.sh
+++ getUpdates.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # This scipt runs every 30 minutes to get and apply updates. Use with CAUTION!
 # Este script executa a cada 30 minutos buscando e aplicando atualizações. Use com CUIDADO!
+
+(echo "BACKUP_BUCKET=$BACKUP_BUCKET"; echo "SUBDOMAIN=$(cat /etc/hostname)"; echo "TZ=$TZ"; echo "DB_PASSWORD=$DB_PASSWORD"; echo "CUSTOMER=$(echo "${CUSTOMER//stage/}")") > /etc/environment
