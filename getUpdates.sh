e8cc3806e72976571519ae57c034f2e373d4b89d
Gustavo B <52040719+Gugabit@users.noreply.github.com>
2022-02-25 17:10:15 -0300

    Update getUpdates.sh

--- getUpdates.sh
+++ getUpdates.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This scipt runs every 30 minutes to get and apply updates. Use with CAUTION!
 # Este script executa a cada 30 minutos buscando e aplicando atualizações. Use com CUIDADO!
-
-wget -qO- https://gitlab.com/Gugabit/tools/-/raw/main/installBitProxy.sh | sh
-
