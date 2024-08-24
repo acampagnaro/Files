722ac65812f06043f7249bd565970b19a21744de
Gustavo B. <52040719+Gugabit@users.noreply.github.com>
2024-08-24 00:43:57 -0300

    Update getUpdates.sh

--- getUpdates.sh
+++ getUpdates.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # This scipt runs every 30 minutes to get and apply updates. Use with CAUTION!
 # Este script executa a cada 30 minutos buscando e aplicando atualizações. Use com CUIDADO!
-
-sudo apt update
-sudo apt upgrade -y
-
-sudo reboot
